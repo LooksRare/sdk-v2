678f5edf39db22d006eaff9ff51e6c14050520ba
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-08-12 16:49:25 +0200

    chore: Update prettier for solidity

--- src/contracts/tests/MockERC721.sol
+++ src/contracts/tests/MockERC721.sol
@@ -5,7 +5,7 @@ import "@rari-capital/solmate/src/tokens/ERC721.sol";
 
 contract MockERC721 is ERC721 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
-    
+
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
     }
