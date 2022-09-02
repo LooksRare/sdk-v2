c5f455aa169f27af1f5dfcb474d6efad78bf45fc
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-02 13:54:58 +0800

    refactor

--- src/__tests__/helpers/eip712.ts
+++ src/__tests__/helpers/eip712.ts
@@ -2,70 +2,84 @@ import { utils } from "ethers";
 import { TypedDataDomain } from "@ethersproject/abstract-signer";
 import { MakerAsk } from "../../types";
 
-export const getMakerOrderHash = (makerOrder: MakerAsk) => {
-  const jsDigest = utils.keccak256(
-    utils.defaultAbiCoder.encode(
-      [
-        "bytes32",
-        "uint112",
-        "uint112",
-        "uint16",
-        "uint8",
-        "uint112",
-        "uint16",
-        "address",
-        "address",
-        "address",
-        "address",
-        "uint256",
-        "uint256",
-        "uint256",
-        "bytes32",
-        "bytes32",
-        "bytes32",
-      ],
-      [
-        "0x85fa30b2b848c94bd5f5b88383658126eb3a69201d0b539f4bf956996bdb6af1",
-        makerOrder.askNonce,
-        makerOrder.subsetNonce,
-        makerOrder.strategyId,
-        makerOrder.assetType,
-        makerOrder.orderNonce,
-        makerOrder.minNetRatio,
-        makerOrder.collection,
-        makerOrder.currency,
-        makerOrder.recipient,
-        makerOrder.signer,
-        makerOrder.startTime,
-        makerOrder.endTime,
-        makerOrder.minPrice,
-        utils.keccak256(utils.solidityPack(["uint256[]"], [makerOrder.itemIds])),
-        utils.keccak256(utils.solidityPack(["uint256[]"], [makerOrder.amounts])),
-        utils.keccak256(makerOrder.additionalParameters),
-      ]
-    )
-  );
-  return jsDigest;
+// Emulate contract cryptographic functions using JS. Used for testing purpose.
+
+/**
+ * Emulate the EIP-712 domain separator computation
+ * @external LooksRareProtocol constructor
+ * @param domain
+ * @returns string (bytes32)
+ */
+export const getDomainSeparator = (domain: TypedDataDomain): string => {
+  const types = ["bytes32", "bytes32", "bytes32", "uint256", "address"];
+  const values = [
+    utils.keccak256(
+      utils.toUtf8Bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
+    ),
+    utils.keccak256(utils.toUtf8Bytes(domain.name!)),
+    utils.keccak256(utils.toUtf8Bytes(domain.version!)),
+    domain.chainId!,
+    domain.verifyingContract!,
+  ];
+  return utils.keccak256(utils.defaultAbiCoder.encode(types, values));
 };
 
-export const getDomainSeparator = (domain: TypedDataDomain) => {
-  return utils.keccak256(
-    utils.defaultAbiCoder.encode(
-      ["bytes32", "bytes32", "bytes32", "uint256", "address"],
-      [
-        utils.keccak256(
-          utils.toUtf8Bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
-        ),
-        utils.keccak256(utils.toUtf8Bytes(domain.name!)),
-        utils.keccak256(utils.toUtf8Bytes(domain.version!)),
-        domain.chainId!,
-        domain.verifyingContract!,
-      ]
-    )
-  );
+/**
+ * Emulate maker order hashing
+ * @external OrderStruct hash function
+ * @param makerOrder
+ * @returns string (bytes32)
+ */
+export const getMakerOrderHash = (makerOrder: MakerAsk): string => {
+  const types = [
+    "bytes32",
+    "uint112",
+    "uint112",
+    "uint16",
+    "uint8",
+    "uint112",
+    "uint16",
+    "address",
+    "address",
+    "address",
+    "address",
+    "uint256",
+    "uint256",
+    "uint256",
+    "bytes32",
+    "bytes32",
+    "bytes32",
+  ];
+  const values = [
+    "0x85fa30b2b848c94bd5f5b88383658126eb3a69201d0b539f4bf956996bdb6af1", // _MAKER_ASK_HASH in OrderStruct.sol
+    makerOrder.askNonce,
+    makerOrder.subsetNonce,
+    makerOrder.strategyId,
+    makerOrder.assetType,
+    makerOrder.orderNonce,
+    makerOrder.minNetRatio,
+    makerOrder.collection,
+    makerOrder.currency,
+    makerOrder.recipient,
+    makerOrder.signer,
+    makerOrder.startTime,
+    makerOrder.endTime,
+    makerOrder.minPrice,
+    utils.keccak256(utils.solidityPack(["uint256[]"], [makerOrder.itemIds])),
+    utils.keccak256(utils.solidityPack(["uint256[]"], [makerOrder.amounts])),
+    utils.keccak256(makerOrder.additionalParameters),
+  ];
+  return utils.keccak256(utils.defaultAbiCoder.encode(types, values));
 };
 
-export const computeDigestMakerAsk = (domain: TypedDataDomain, makerOrder: MakerAsk) => {
+/**
+ * Emulate digest computation
+ * @external LooksRareProtocol _computeDigestAndVerify function
+ * @param domain
+ * @param makerOrder
+ * @returns string (bytes32)
+ */
+export const computeDigestMakerAsk = (domain: TypedDataDomain, makerOrder: MakerAsk): string => {
   const domainSeparator = getDomainSeparator(domain);
   const hash = getMakerOrderHash(makerOrder);
   return utils.keccak256(utils.solidityPack(["string", "bytes32", "bytes32"], ["\x19\x01", domainSeparator, hash]));
--- src/contracts/tests/Verifier.sol
+++ src/contracts/tests/Verifier.sol
@@ -2,23 +2,17 @@
 pragma solidity ^0.8.14;
 
 import "contracts-exchange-v2/contracts/LooksRareProtocol.sol";
-
+import "contracts-exchange-v2/contracts/LooksRareProtocolHelpers.sol";
 import "hardhat/console.sol";
 
-contract Verifier is SignatureChecker {
+contract Verifier is LooksRareProtocolHelpers {
     using OrderStructs for OrderStructs.MakerAsk;
     using OrderStructs for OrderStructs.MakerBid;
     using OrderStructs for bytes32;
 
-    LooksRareProtocol public immutable looksRareProtocol;
+    constructor(address _looksRareProtocol) LooksRareProtocolHelpers(_looksRareProtocol) {}
 
-    string internal constant _ENCODING_PREFIX = "\x19\x01";
-
-    constructor(address _looksRareProtocol) {
-        looksRareProtocol = LooksRareProtocol(_looksRareProtocol);
-    }
-
-    function getMakerOrderHash(OrderStructs.MakerAsk memory makerAsk) public view returns (bytes32 orderHash) {
+    function getMakerOrderHash(OrderStructs.MakerAsk memory makerAsk) public pure returns (bytes32 orderHash) {
         return makerAsk.hash();
     }
 
@@ -27,19 +21,13 @@ contract Verifier is SignatureChecker {
         return domainSeparator;
     }
 
-    function computeDigestMakerAsk(OrderStructs.MakerAsk memory makerAsk) public view returns (bytes32 digest) {
-        (, , bytes32 domainSeparator, ) = looksRareProtocol.information();
-        return keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, makerAsk.hash()));
-    }
-
     function verifyAskOrders(OrderStructs.MakerAsk calldata makerAsk, bytes calldata signature) external view {
         bytes32 digest = computeDigestMakerAsk(makerAsk);
         _verify(digest, makerAsk.signer, signature);
     }
 
     function verifyBidOrders(OrderStructs.MakerBid calldata makerBid, bytes calldata signature) external view {
-        (, , bytes32 domainSeparator, ) = looksRareProtocol.information();
-        bytes32 digest = keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, makerBid.hash()));
+        bytes32 digest = computeDigestMakerBid(makerBid);
         _verify(digest, makerBid.signer, signature);
     }
 }
