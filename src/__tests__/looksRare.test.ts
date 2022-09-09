aef26dfaeac01a7755e302253ccb842dac253703
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-09 14:58:38 +0800

    chore: Override addresses for tests

--- src/__tests__/looksRare.test.ts
+++ src/__tests__/looksRare.test.ts
@@ -1,10 +1,23 @@
 import { expect } from "chai";
+import { constants } from "ethers";
+import { setUpContracts, Mocks } from "./helpers/setup";
 import { LooksRare } from "../LooksRare";
 import { SupportedChainId } from "../types";
+import { Addresses } from "../constants/addresses";
 
 describe("LooksRare class", () => {
-  it("instanciate LooksRare with the correct chain id", () => {
+  let contracts: Mocks;
+  beforeEach(async () => {
+    contracts = await setUpContracts();
+  });
+  it("instanciate LooksRare object", () => {
     expect(new LooksRare(1).chainId).to.equal(1);
-    expect(new LooksRare(SupportedChainId.GOERLI).chainId).to.equal(SupportedChainId.GOERLI);
+    expect(new LooksRare(SupportedChainId.HARDHAT).chainId).to.equal(SupportedChainId.HARDHAT);
+    const addresses: Addresses = {
+      EXCHANGE: contracts.looksRareProtocol.address,
+      LOOKS: constants.AddressZero,
+      TRANSFER_MANAGER: contracts.transferManager.address,
+    };
+    expect(new LooksRare(SupportedChainId.HARDHAT, addresses).addresses).to.be.eql(addresses);
   });
 });
