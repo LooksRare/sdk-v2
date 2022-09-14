75ce25c98c96ff38fb175beed68d01fd7436f8c9
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-14 17:19:18 +0800

    feat: Add analysis methods

--- src/utils/calls/exchange.ts
+++ src/utils/calls/exchange.ts
@@ -3,6 +3,21 @@ import { LooksRareProtocol } from "../../../typechain/contracts-exchange-v2/cont
 import abiLooksRareProtocol from "../../abis/LooksRareProtocol.json";
 import { MakerAsk, MakerBid, TakerAsk, TakerBid, Signer } from "../../types";
 
+const getWriteContractMethods = (contract: Contract, method: string, args: any, initialOverrides: Overrides) => {
+  return {
+    call: (overrides: Overrides) =>
+      contract[method](...args, {
+        ...initialOverrides,
+        ...overrides,
+      }),
+    estimateGas: (overrides: Overrides) =>
+      contract.estimateGas[method](...args, {
+        ...initialOverrides,
+        ...overrides,
+      }),
+  };
+};
+
 export const executeTakerBid = (
   signer: Signer,
   address: string,
