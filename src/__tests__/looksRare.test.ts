a97b937f2297175eb8fd1dc0c739299bed72d175
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-14 16:04:33 +0800

    feat: Add signer to the constructor

--- src/__tests__/looksRare.test.ts
+++ src/__tests__/looksRare.test.ts
@@ -1,23 +1,25 @@
 import { expect } from "chai";
 import { constants } from "ethers";
-import { setUpContracts, Mocks } from "./helpers/setup";
+import { setUpContracts, Mocks, getSigners, Signers } from "./helpers/setup";
 import { LooksRare } from "../LooksRare";
 import { SupportedChainId } from "../types";
 import { Addresses } from "../constants/addresses";
 
 describe("LooksRare class", () => {
   let contracts: Mocks;
+  let signers: Signers;
   beforeEach(async () => {
     contracts = await setUpContracts();
+    signers = await getSigners();
   });
   it("instanciate LooksRare object", () => {
-    expect(new LooksRare(1).chainId).to.equal(1);
-    expect(new LooksRare(SupportedChainId.HARDHAT).chainId).to.equal(SupportedChainId.HARDHAT);
+    expect(new LooksRare(signers.user1, 1).chainId).to.equal(1);
+    expect(new LooksRare(signers.user1, SupportedChainId.HARDHAT).chainId).to.equal(SupportedChainId.HARDHAT);
     const addresses: Addresses = {
       EXCHANGE: contracts.looksRareProtocol.address,
       LOOKS: constants.AddressZero,
       TRANSFER_MANAGER: contracts.transferManager.address,
     };
-    expect(new LooksRare(SupportedChainId.HARDHAT, addresses).addresses).to.be.eql(addresses);
+    expect(new LooksRare(signers.user1, SupportedChainId.HARDHAT, addresses).addresses).to.be.eql(addresses);
   });
 });
