2d02c43653ed57e5df3f2b2554e4efb2bdeac180
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-15 13:39:47 +0800

    build: Setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import copy from "rollup-plugin-copy";
+import bundleSize from "rollup-plugin-bundle-size";
+import pkg from "./package.json";
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: "src/index.ts",
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
+  ],
+  plugins: [
+    copy({
+      targets: [{ src: "src/abis/**/*", dest: "dist/abis" }],
+    }),
+    json(),
+    typescript({ module: "ESNext" }),
+    bundleSize(),
+  ],
+};
