fcf9e598bc8065f55d1a6179713a4a043d64b33a
Shisui <90075884+0xShisui@users.noreply.github.com>
2022-09-26 15:48:13 +0900

    test: Create taker orders (#48)

---
+++ src/__tests__/looksrare/createTaker.test.ts
@@ -0,0 +1,81 @@
+import { expect } from "chai";
+import { constants, utils } from "ethers";
+import { ethers } from "hardhat";
+import { setUpContracts, Mocks, getSigners, Signers } from "../helpers/setup";
+import { LooksRare } from "../../LooksRare";
+import { Addresses } from "../../constants/addresses";
+import {
+  SupportedChainId,
+  AssetType,
+  StrategyType,
+  MakerAskInputs,
+  MakerBidInputs,
+  TakerBid,
+  TakerAsk,
+} from "../../types";
+
+describe("Create maker ask", () => {
+  let contracts: Mocks;
+  let signers: Signers;
+  let addresses: Addresses;
+  beforeEach(async () => {
+    contracts = await setUpContracts();
+    signers = await getSigners();
+    addresses = {
+      EXCHANGE: contracts.looksRareProtocol.address,
+      LOOKS: constants.AddressZero,
+      TRANSFER_MANAGER: contracts.transferManager.address,
+      WETH: contracts.weth.address,
+    };
+  });
+  it("create taker bid", async () => {
+    const baseMakerAskInput: MakerAskInputs = {
+      collection: contracts.collection1.address,
+      assetType: AssetType.ERC721,
+      strategyId: StrategyType.standard,
+      subsetNonce: 0,
+      orderNonce: 0,
+      startTime: Math.floor(Date.now() / 1000),
+      endTime: Math.floor(Date.now() / 1000) + 3600,
+      price: utils.parseEther("1"),
+      itemIds: [1],
+    };
+    const looksrare = new LooksRare(signers.user1, ethers.provider, SupportedChainId.HARDHAT, addresses);
+    const { order } = await looksrare.createMakerAsk(baseMakerAskInput);
+    const takerBid = looksrare.createTakerBid(order, signers.user2.address);
+    const expectedTakerBid: TakerBid = {
+      recipient: signers.user2.address,
+      itemIds: order.itemIds,
+      amounts: order.amounts,
+      maxPrice: order.minPrice,
+      minNetRatio: order.minNetRatio,
+      additionalParameters: "0x",
+    };
+    await expect(takerBid).to.be.eql(expectedTakerBid);
+  });
+  it("create taker ask", async () => {
+    const baseMakerAskInput: MakerBidInputs = {
+      collection: contracts.collection1.address,
+      assetType: AssetType.ERC721,
+      strategyId: StrategyType.standard,
+      subsetNonce: 0,
+      orderNonce: 0,
+      startTime: Math.floor(Date.now() / 1000),
+      endTime: Math.floor(Date.now() / 1000) + 3600,
+      price: utils.parseEther("1"),
+      itemIds: [1],
+    };
+    const looksrare = new LooksRare(signers.user1, ethers.provider, SupportedChainId.HARDHAT, addresses);
+    const { order } = await looksrare.createMakerBid(baseMakerAskInput);
+    const takerAsk = looksrare.createTakerAsk(order, signers.user2.address);
+    const expectedTakerAsk: TakerAsk = {
+      recipient: signers.user2.address,
+      itemIds: order.itemIds,
+      amounts: order.amounts,
+      minPrice: order.maxPrice,
+      minNetRatio: order.minNetRatio,
+      additionalParameters: "0x",
+    };
+    await expect(takerAsk).to.be.eql(expectedTakerAsk);
+  });
+});
