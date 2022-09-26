af95e5e1b8a98c7d8d3f543d82e7a3fb30a07275
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-09-26 15:36:08 +0800

    test: Execute taker bid

---
+++ src/__tests__/looksrare/executeTakerBid.test.ts
@@ -0,0 +1,52 @@
+import { expect } from "chai";
+import { constants, utils } from "ethers";
+import { ethers } from "hardhat";
+import { setUpContracts, Mocks, getSigners, Signers } from "../helpers/setup";
+import { LooksRare } from "../../LooksRare";
+import { Addresses } from "../../constants/addresses";
+import { SupportedChainId, AssetType, StrategyType, MakerAskInputs } from "../../types";
+
+describe("execute taker bid", () => {
+  let contracts: Mocks;
+  let signers: Signers;
+  let baseMakerAskInput: MakerAskInputs;
+  let addresses: Addresses;
+  beforeEach(async () => {
+    contracts = await setUpContracts();
+    signers = await getSigners();
+
+    const tx = await contracts.transferManager
+      .connect(signers.user1)
+      .grantApprovals([contracts.looksRareProtocol.address]);
+    await tx.wait();
+
+    addresses = {
+      EXCHANGE: contracts.looksRareProtocol.address,
+      LOOKS: constants.AddressZero,
+      TRANSFER_MANAGER: contracts.transferManager.address,
+      WETH: contracts.weth.address,
+    };
+    baseMakerAskInput = {
+      collection: contracts.collection1.address,
+      assetType: AssetType.ERC721,
+      strategyId: StrategyType.standard,
+      subsetNonce: 0,
+      orderNonce: 0,
+      startTime: Math.floor(Date.now() / 1000),
+      endTime: Math.floor(Date.now() / 1000) + 3600,
+      price: utils.parseEther("1"),
+      itemIds: [1],
+    };
+  });
+  it("execute maker ask and taker bid", async () => {
+    const lrUser1 = new LooksRare(signers.user1, ethers.provider, SupportedChainId.HARDHAT, addresses);
+    const lrUser2 = new LooksRare(signers.user2, ethers.provider, SupportedChainId.HARDHAT, addresses);
+    const { order, action } = await lrUser1.createMakerAsk(baseMakerAskInput);
+    await action!();
+    const signature = await lrUser1.signMakerAsk(order);
+
+    const takerBid = lrUser2.createTakerBid(order, signers.user2.address);
+    const receipt = await lrUser2.executeTakerBid(order, takerBid, signature);
+    expect(receipt.status).to.be.equal(1);
+  });
+});
--- src/contracts/tests/MockERC721.sol
+++ src/contracts/tests/MockERC721.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.7;
 
-import "@rari-capital/solmate/src/tokens/ERC721.sol";
+import "solmate/src/tokens/ERC721.sol";
 
 contract MockERC721 is ERC721 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
