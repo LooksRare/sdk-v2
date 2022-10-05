14a626efd5e64df24872f3050cd2388391df8fd3
Shisui <90075884+0xShisui@users.noreply.github.com>
2022-10-05 14:34:19 +0900

    chore: Add missing imports (#56)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 export { addressesByNetwork } from "./constants/addresses";
 export { chainInfo } from "./constants/chains";
+export * as eip712 from "./constants/eip712";
+export * from "./constants";
 
-export { SupportedChainId } from "./types";
+export * from "./types";
+
+import * as tokens from "./utils/calls/tokens";
+import * as encode from "./utils/encodeOrderParams";
+const utils = {
+  ...tokens,
+  ...encode,
+};
+export { utils };
 
 export { LooksRare } from "./LooksRare";
