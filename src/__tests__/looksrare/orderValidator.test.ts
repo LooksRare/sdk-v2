f71ec23a0987f5a401ecd265944a8c6ad1286ab7
Shisui <90075884+0xShisui@users.noreply.github.com>
2023-03-07 08:19:51 +0100

    test: Multiple orders validation (#113)

--- src/__tests__/looksrare/orderValidator.test.ts
+++ src/__tests__/looksrare/orderValidator.test.ts
@@ -8,17 +8,13 @@ import { SupportedChainId, CollectionType, StrategyType, CreateMakerInput, Order
 describe("Order validation", () => {
   let mocks: SetupMocks;
   let signers: Signers;
+  let baseMakerAskInput: CreateMakerInput;
+  let baseMakerBidInput: CreateMakerInput;
   beforeEach(async () => {
     mocks = await setUpContracts();
     signers = await getSigners();
 
-    const tx = await mocks.contracts.transferManager
-      .connect(signers.user1)
-      .grantApprovals([mocks.addresses.EXCHANGE_V2]);
-    await tx.wait();
-  });
-  it("verify maker ask orders", async () => {
-    const baseMakerAskInput: CreateMakerInput = {
+    baseMakerAskInput = {
       collection: mocks.contracts.collectionERC721.address,
       collectionType: CollectionType.ERC721,
       strategyId: StrategyType.standard,
@@ -29,6 +25,24 @@ describe("Order validation", () => {
       price: utils.parseEther("1"),
       itemIds: [1],
     };
+
+    baseMakerBidInput = {
+      collection: mocks.contracts.collectionERC721.address,
+      collectionType: CollectionType.ERC721,
+      strategyId: StrategyType.standard,
+      subsetNonce: 0,
+      orderNonce: 0,
+      endTime: Math.floor(Date.now() / 1000) + 3600,
+      price: utils.parseEther("1"),
+      itemIds: [1],
+    };
+
+    const tx = await mocks.contracts.transferManager
+      .connect(signers.user1)
+      .grantApprovals([mocks.addresses.EXCHANGE_V2]);
+    await tx.wait();
+  });
+  it("verify maker ask orders", async () => {
     const lr = new LooksRare(SupportedChainId.HARDHAT, ethers.provider, signers.user1, mocks.addresses);
     const { maker } = await lr.createMakerAsk(baseMakerAskInput);
     const signature = await lr.signMakerOrder(maker);
@@ -43,17 +57,6 @@ describe("Order validation", () => {
     expect(orders[0].every((code) => code === OrderValidatorCode.ORDER_EXPECTED_TO_BE_VALID)).to.be.true;
   });
   it("verify maker bid orders", async () => {
-    const baseMakerBidInput: CreateMakerInput = {
-      collection: mocks.contracts.collectionERC721.address,
-      collectionType: CollectionType.ERC721,
-      strategyId: StrategyType.standard,
-      subsetNonce: 0,
-      orderNonce: 0,
-      endTime: Math.floor(Date.now() / 1000) + 3600,
-      price: utils.parseEther("1"),
-      itemIds: [1],
-    };
-
     const lr = new LooksRare(SupportedChainId.HARDHAT, ethers.provider, signers.user1, mocks.addresses);
     const { maker } = await lr.createMakerBid(baseMakerBidInput);
     const signature = await lr.signMakerOrder(maker);
@@ -68,5 +71,25 @@ describe("Order validation", () => {
 
     orders = await lr.verifyMakerOrders([maker], [signature]);
     expect(orders[0].every((code) => code === OrderValidatorCode.ORDER_EXPECTED_TO_BE_VALID)).to.be.true;
+  });
+  it("verify multiple maker orders with a Merkle tree", async () => {
+    const lr = new LooksRare(SupportedChainId.HARDHAT, ethers.provider, signers.user1, mocks.addresses);
+    const { maker: makerBid } = await lr.createMakerBid(baseMakerBidInput);
+    const { maker: makerAsk } = await lr.createMakerAsk(baseMakerAskInput);
+    const { signature, merkleTreeProofs } = await lr.signMultipleMakerOrders([makerBid, makerAsk]);
+
+    let tx = await lr.approveAllCollectionItems(baseMakerAskInput.collection);
+    await tx.wait();
+    tx = await lr.approveErc20(lr.addresses.WETH);
+    await tx.wait();
+
+    const orders = await lr.verifyMakerOrders(
+      [makerBid, makerAsk],
+      [signature, signature],
+      [merkleTreeProofs[0], merkleTreeProofs[1]]
+    );
+
+    expect(orders[0].every((code) => code === OrderValidatorCode.ORDER_EXPECTED_TO_BE_VALID)).to.be.true;
+    expect(orders[1].every((code) => code === OrderValidatorCode.ORDER_EXPECTED_TO_BE_VALID)).to.be.true;
   });
 });
