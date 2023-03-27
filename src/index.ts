46d448ab0128ccc041b89044c5de0c4196557d69
0xShisui <90075884+0xShisui@users.noreply.github.com>
2023-03-27 18:24:53 +0800

    chore: Expose tree height helper

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import * as orderValidator from "./utils/calls/orderValidator";
 import * as strategies from "./utils/calls/strategies";
 import * as encode from "./utils/encodeOrderParams";
 import * as signMakerOrders from "./utils/signMakerOrders";
+import { Eip712MakerMerkleTree } from "./utils/Eip712MakerMerkleTree";
 import * as eip712 from "./constants/eip712";
 const utils = {
   calls: {
@@ -21,6 +22,7 @@ const utils = {
   eip712,
   ...encode,
   ...signMakerOrders,
+  Eip712MakerMerkleTree,
 };
 export { utils };
 
