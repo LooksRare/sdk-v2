109bf7bce7495006ae3bc817b1c1321d30ef4801
Nirato <73018676+niratodev@users.noreply.github.com>
2023-12-08 16:19:00 -0600

    chore: Add legacy types (#183)

--- src/types.ts
+++ src/types.ts
@@ -302,3 +302,30 @@ export enum OrderValidatorCode {
   BUNDLE_ERC2981_NOT_SUPPORTED = 901,
   CREATOR_FEE_TOO_HIGH = 902,
 }
+
+// Legacy types from V1 still in use
+
+/**
+ * /!\ This type is used for the signature and should perfectly match the object signed by the user
+ * Do not update unless the contract has been updated
+ */
+export interface MakerOrder_V1 {
+  isOrderAsk: boolean; // true --> ask / false --> bid
+  signer: string; // signer address of the maker order
+  collection: string; // collection address
+  price: BigNumberish;
+  tokenId: BigNumberish; // id of the token
+  amount: BigNumberish; // amount of tokens to sell/purchase (must be 1 for ERC721, 1+ for ERC1155)
+  strategy: string; // strategy for trade execution (e.g., DutchAuction, StandardSaleForFixedPrice)
+  currency: string; // currency address
+  nonce: BigNumberish; // order nonce (must be unique unless new maker order is meant to override existing one e.g., lower ask price)
+  startTime: BigNumberish; // startTime in timestamp
+  endTime: BigNumberish; // endTime in timestamp
+  minPercentageToAsk: BigNumberish;
+  params: any[]; // params (e.g., price, target account for private sale)
+}
+
+/** MakerOrderWithSignature matches the type used for API order mutations and contract calls. */
+export interface MakerOrderWithSignature_V1 extends MakerOrder_V1 {
+  signature: string;
+}
